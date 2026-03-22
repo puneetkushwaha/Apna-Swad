c0ffcb0df8d171a5df3676aa363ac0da8cbfd1ef
Puneet Kushwaha <148247654+puneetkushwaha@users.noreply.github.com>
2026-03-22 10:15:32 +0530

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 import { Routes, Route, useLocation } from 'react-router-dom';
 import Header from './components/Header/Header';
 import Footer from './components/Footer/Footer';
